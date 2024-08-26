5653bd91f6699cbcbfc2ab06cbdfef868632befb
Artem Sitnov <stnoov@gmail.com>
2024-08-26 11:19:48 +0200

    Update en.js

--- src/lang/en.js
+++ src/lang/en.js
@@ -30,6 +30,10 @@ export const messagesInEnglish = {
   "fields.completed_lessons": "Completed lessons",
   "fields.account_balance": "Account balance",
   "fields.latest_news": "Latest news",
+  "fields.elementary": "Kids 9-11 years old",
+  "fields.pre_intermediate": "Kids 10-13 years old",
+  "fields.intermediate": "Teens 12+ years old",
+  "fields.adults": "Adults",
   "actions.choose_your_group": "Choose your group",
 
   "promotions.promotions_bonuses": "OkieDokie! club loyalty program",
@@ -46,14 +50,17 @@ export const messagesInEnglish = {
   "promotions.how_to_obtain_classes_desc": `Just let the organizer of the club know about it in any way convenient for you - either at the event or in a personal message
 The organizer usually asks the participants themselves if it is time to exchange the earned bonuses for the classes.`,
 
-  "group.elementary": "«Elementary» (A1) 11+ years old",
-  "group.elementary_description": `Can understand and use familiar everyday expressions and very basic phrases aimed at the satisfaction of needs of a concrete type. `,
+  "group.elementary": "Kids 9-11 years old",
+  "group.elementary_description": `You know simple words and phrases, and can read short texts. `,
 
-  "group.pre_intermediate": "«Pre-intermediate» (А2) 13+ years old",
-  "group.pre_intermediate_description": `Can communicate in simple and routine tasks`,
+  "group.pre_intermediate": "Kids 10-13 years old",
+  "group.pre_intermediate_description": `You know how to use basic words and phrases, communicate on simple topics, and read short texts.`,
 
-  "group.intermediate": "«Intermediate» ",
-  "group.intermediate_description": `Can deal with most situations likely to arise while travelling in an area where the language is spoken.`,
+  "group.intermediate": "Teens 12+ years old",
+  "group.intermediate_description": `You can maintain conversations on simple topics, construct more complex sentences, and read longer texts.`,
+
+  "group.adults": "Adults",
+  "group.adults_description": `You can communicate on everyday topics, construct complex sentences.`,
 
   "lesson.successful_signup": `The meeting has been successfully booked!`,
   "lesson.successful_signup_description": `The link was sent to your e-mail address. You can also join us using the link on the main page of the web-site available 15 minutes before the meeting starts.`,
