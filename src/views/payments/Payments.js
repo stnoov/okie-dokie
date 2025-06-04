23973c6f579eab3aa159b2da810329a37089aee8
Artem Sitnov <stnoov@gmail.com>
2025-06-04 20:51:49 +0200

    payment: update payment view

--- src/views/payments/Payments.js
+++ src/views/payments/Payments.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Grid, Typography, makeStyles, withWidth } from "@material-ui/core";
-import { useIntl } from "react-intl";
+import { Grid, makeStyles, withWidth } from "@material-ui/core";
 import PaymentForm from "../../components/payments/PaymentForm";
 
 const useStyles = makeStyles((theme) => ({
@@ -19,42 +18,13 @@ const useStyles = makeStyles((theme) => ({
 
 function Payments({ width }) {
   const classes = useStyles();
-  const intl = useIntl();
   return (
     <>
       <Grid container className={classes.rootContainer}>
         <Grid item xs={12}>
-          <Typography variant="h3" color="secondary">
-            {intl.formatMessage({
-              id: "routes.payments",
-              defaultMessage: "Payments",
-            })}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Typography
-                variant="body2"
-                color="primary"
-                className={classes.paymentText}
-              >
-                Оплачивая занятия в OkieDokie! club, вы приобретаете отличную
-                возможность попрактиковаться в разговорном английском, а также
-                помогаете нашей команде!
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
           <PaymentForm />
         </Grid>
-        <Grid item xs={12}>
-          <Typography variant="body2" color="textSecondary">
-            Платежи осуществляются через сторонний сервис электронных платежей
-            ЮMoney.
-          </Typography>
-        </Grid>
+
       </Grid>
     </>
   );
